7612a387ebc3ae9721e57664c7b40c7d6acf2904
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-04-21 13:46:15 -0600

    added Llist.py which is a pre-built linked_list data structure

--- a_star.py
+++ a_star.py
@@ -1,7 +1,5 @@
 
 
-
-
 def distance_calculator(current,goal):
     """Purpose: calculates the manhattin distance between 2 points
     :param current: a tuple of x,y coordinates representing the current path
@@ -12,3 +10,14 @@ def distance_calculator(current,goal):
     end_x,end_y = goal
     F = (end_x+end_y) - (x+y)
     return abs(F)
+
+
+
+open_vertices = {
+    #(x,y):(lowest_F,previous_vertex)
+}
+
+closed_vertices = []
+
+def get_open_vertices(current,table):
+    '''Purpose: will grab'''
