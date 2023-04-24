7f402ac66ac3c5ee32ba2039606bf6ae6f1466fe
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-04-24 11:24:19 -0600

    Created tests for square_generator function

--- a_star_tests.py
+++ a_star_tests.py
@@ -85,9 +85,38 @@ if not (check1 and check2):
     print("error checking if get_current works")
 
 
+##########################################################
+#test square generator
 
 
 
 
 
 
+def test_square_generator(dict):
+    '''Purpose: to test the square_generator function
+    :param dict: a dictionary containing the test cases,expected outputs and reasons for testing
+    :return: None
+    Post-conditions: will print error on console if expected !=actual output'''
+
+    grid  = [[n for n in range(10)] for m in range(10)]
+    for test in dict:
+        current = test[0]
+        taken = test[1]
+        
+        actual = A.square_generator(current,grid,taken)
+        expected = dict[test][0]
+        reason = dict[test][1]
+        if actual != expected:
+            print(f"error with input {test} actual was {actual}, expected was {expected},reason:{reason}")
+
+
+
+square_generator_tests = {
+    #(current,taken):(expected,reason)
+    ((5,4),()):([(4,3),(4,4),(4,5),(5,3),(5,4),(5,5),(6,3),(6,4),(6,5)],"check arbituary point"),
+    ((-5,-4),()):([],"check out of bounds"),
+    ((5,4),((4,3),(5,4))):([(4,4),(4,5),(5,3),(5,5),(6,3),(6,4),(6,5)],"check invalid spaces do not appear in list of valid points"),
+}
+
+test_square_generator(square_generator_tests)
