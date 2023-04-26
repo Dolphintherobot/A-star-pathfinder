ed3fd9f7ed1959c492f9588cbc105d21493efe44
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-04-26 11:12:59 -0600

    Created tests for find_path function

--- a_star_tests.py
+++ a_star_tests.py
@@ -153,8 +153,9 @@ def test_find_path(dict):
     :return: None
     Post-conditions: will print error on console if expected !=actual output'''
 
-    grid  = [[n for n in range(10)] for m in range(10)]
+    
     for test in dict:
+        grid  = [[0 for n in range(10)] for m in range(10)]
         start = test[0]
         end = test[1]
 
@@ -177,3 +178,101 @@ find_path_tests = {
 }
 
 test_find_path(find_path_tests)
+
+
+
+
+
+
+def test_find_path2(dict):
+    '''Purpose: to test the find_path function,this time with walls involved
+    :param dict: a dictionary containing the test cases,expected outputs and reasons for testing
+    :return: None
+    Post-conditions: will print error on console if expected !=actual output'''
+
+    
+    for test in dict:
+        grid  = [[0 for n in range(10)] for m in range(10)]
+        start = test[0]
+        end = test[1]
+        invalid_spaces = test[2]
+        for space in invalid_spaces:
+            x,y = space
+            grid[x][y] = 2
+            # if 0 < x <8:
+            #     grid[x+1][y] = 2
+            #     grid[9][1] = 2
+                
+
+
+        
+        actual = A.find_path(grid,start,end)
+        expected = dict[test][0]
+        reason = dict[test][1]
+        if actual != expected:
+            print(f"error with input {test} actual was {actual}, expected was {expected},reason:{reason}")
+
+
+negative_diagonal = ((9,0),(8,1),(7,2),(6,3),(5,4),(4,5),(3,6),(2,7),(1,8),(0,9))
+postive_diagonal = ((x,x) for x in range(10))
+random_walls = ((5,5),(2,1),(5,6),(4,6),(1,6))
+
+find_path_tests2 = {
+    #(start,end,invalid_spaces):(expected,reason)
+    ((0,0),(9,9),negative_diagonal):(False,"blocking off any possible entry to end"),
+    ((0,0),(9,9),postive_diagonal):(False,"starting on a wall"),
+    ((9,9),(0,0),random_walls):(True,"path open with walls blocking shortest path")
+
+}
+
+test_find_path2(find_path_tests2)
+
+
+
+
+def test_find_path3(dict):
+    '''Purpose: to test the find_path function,this time with a rectangular grid
+    :param dict: a dictionary containing the test cases,expected outputs and reasons for testing
+    :return: None
+    Post-conditions: will print error on console if expected !=actual output'''
+
+    
+    for test in dict:
+        grid  = [[0 for n in range(5)] for m in range(4)]
+        
+        start = test[0]
+        end = test[1]
+        invalid_spaces = test[2]
+        for space in invalid_spaces:
+            x,y = space
+            
+            grid[x][y] = 2
+            # if 0 < x <8:
+            #     grid[x+1][y] = 2
+            #     grid[9][1] = 2
+                
+
+
+        
+        actual = A.find_path(grid,start,end)
+        expected = dict[test][0]
+        reason = dict[test][1]
+        if actual != expected:
+            print(f"error with input {test} actual was {actual}, expected was {expected},reason:{reason}")
+
+
+
+negative_diagonal = ((0,4),(1,3),(2,2),(3,1))
+postive_diagonal = ((x,x) for x in range(4))
+random_walls = ((2,4),(2,1),(3,2),(1,1))
+
+find_path_tests3 = {
+    #(start,end,invalid_spaces):(expected,reason)
+    ((0,0),(4,3),negative_diagonal):(False,"blocking off any possible entry to end"),
+    ((0,0),(4,3),postive_diagonal):(False,"starting on a wall"),
+    ((0,0),(3,4),random_walls):(True,"path open with walls blocking shortest path")
+
+}
+
+
+test_find_path3(find_path_tests3)
