41e3f6169f8fcf7fae1b12068321b03472ddcb23
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-04-30 09:13:51 -0600

    moved the main program loop into function called main.Change was made to improve readability and maintainability

--- array_backed_grid.py
+++ array_backed_grid.py
@@ -53,54 +53,45 @@ def draw_squares(screen,grid,height,width,margin):
                               width,
                               height])       
 
+
+
+def main(grid):
+    '''
+    Purpose:program that runs the gui
+    :param grid: a 2d list'''
+    pygame.init()
+    size = (255, 255)
+    screen = pygame.display.set_mode(size)
+
+    height = 20
+    width = 20    
+    margin = 5
+    
+    done = False
+    clock = pygame.time.Clock()
+
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type ==pygame.MOUSEBUTTONDOWN:
+                mouse_x,mouse_y = get_coordinates()
+                map_to_grid(grid,width,margin,mouse_x,mouse_y)
+
+        BLACK = (0,0,0)
+        screen.fill(BLACK)
+        draw_squares(screen,grid,height,width,margin)
+        pygame.display.flip()
+    
+        clock.tick(60)
  
-#some colors defined in RGB
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
-pygame.init()
- 
-#size of squares and screen
-height = 20
-width = 20    
-margin = 5
-size = (255, 255)
-screen = pygame.display.set_mode(size)
+
+    pygame.quit()
 
 
 n = 10 #how many rows 
 m = 10 #how many columns
- 
-pygame.display.set_caption("My Game")
 grid  = [[0 for x in range(n)] for y in range(m)]
 
-done = False
- 
-clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type ==pygame.MOUSEBUTTONDOWN:
-            mouse_x,mouse_y = get_coordinates()
-            map_to_grid(grid,width,margin,mouse_x,mouse_y)
-
-    
-
-    screen.fill(BLACK)
-    draw_squares(screen,grid,height,width,margin)
-
-    
-    
-    
-    pygame.display.flip()
- 
-    clock.tick(60)
- 
-
-pygame.quit()
-
+main(grid)
